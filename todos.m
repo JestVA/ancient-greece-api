f333a606864b469eb97215188c54639a9100f263
dori <d.dumitrascuta@gmail.com>
2019-08-29 23:30:33 +0100

    added a bunch of goals 👨🏽‍💻

--- todos.m
+++ todos.m
@@ -1,2 +1,7 @@
 // possibly copy node_modules folder to GitHub repo
 // Let's see if linking git worked - to allow for code changes directly and file creation to droplet
+// good to see how I created right folder structure on the server 🚀
+// and git flow complete - 🛳
+// TODO; create waiting page in Front End  , add React, possible Parcel, Babel, setup easy-cluster DB on DigitalOcean, gulp ?! auto run app on server start ?? nodemon // script // routes with get // koa-router 
+// good practice to include Ancient Greece data in relational database mySQL vs GraphQL
+// making game in console while here (interactive) // ability to sign up via CLI for when release 
